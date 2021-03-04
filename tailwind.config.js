28578a7e9592328f0662e18d789bc5ee2eed02f4
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-03-04 02:08:32 -0600

    🚀 Deploy site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anomaly: 'Accuratist, sans-serif',
+      }
+    },
   },
   variants: {
     extend: {},
